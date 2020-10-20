5bcb16277073bc5b8e508093972a0e181f49a154
zonghua2016 <2280314971@qq.com>
2020-10-21 03:05:41 +0800

    更换模板git地址

--- src/constans.js
+++ src/constans.js
@@ -2,11 +2,11 @@
  * @Author       : tongzonghua
  * @Date         : 2020-10-20 22:56:14
  * @LastEditors  : tongzonghua
- * @LastEditTime : 2020-10-21 02:40:41
+ * @LastEditTime : 2020-10-21 03:04:00
  * @Email        : tongzonghua@360.cn
  * @Description  : 
  * @FilePath     : /cli/aggna-cli/src/constans.js
  */
 
-exports.TEMPLATE_GET_REPO = 'direct:https://github.com/zonghua2016/QPic.git';
+exports.TEMPLATE_GET_REPO = 'direct:https://github.com/zonghua2016/aggna-electron-template.git';
 exports.INJECT_FILES = ['package.json']
